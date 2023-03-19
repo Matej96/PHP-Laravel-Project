698c99ea25b632534280f075527fd3f3a06c827b
Lukáš <xrucek@stuba.sk>
2023-03-19 20:07:34 +0100

    Order_Shipping_Payment_Page_1.0

--- script/script_order_shipping_payment.js
+++ script/script_order_shipping_payment.js
@@ -1,10 +1,14 @@
+/* Script slúži na zakrytie/zobrazenie sekcie "Dodacie údaje" */
+
 var checkBox = document.getElementById("fakturacne_check");
 var text = document.getElementById("dodacie");
 
-checkBox.addEventListener('change', function() {
-    if (checkBox.checked == true){
+checkBox.addEventListener('change', function () {
+    if (checkBox.checked == true) {
         text.style.display = "none";
     } else {
-        text.style.display = "block";
+        text.style.display = "flex";
     }
 })
+
+/* Koniec Scriptu */
