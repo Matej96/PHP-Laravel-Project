114fcfb98eda9c35e790009744ddbc1f8693157a
Adamtomcala <xtomcala@stuba.sk>
2023-03-20 14:07:23 +0100

    uprava navbaru

--- script/navbar.js
+++ script/navbar.js
@@ -1,31 +1,59 @@
 /* Navbar Script */
 
 $(document).ready(function () {
-    $(".woman-section").hover(() => {
-        $("#womanSection").addClass("active");
+    $(".woman-section").click(() => {
+        if($("#manSection").hasClass("active")){
+            $("#manSection").removeClass("active");
+        }else if ($("#kidsSection").hasClass("active")){
+            $("#kidsSection").removeClass("active");
+        }
+        
+        if($("#womanSection").hasClass("active")){
+            $("#womanSection").removeClass("active");
+        }else{
+            $("#womanSection").addClass("active");
+        }
     });
 
-    $(".woman-section").mouseleave(() => {
-        $("#womanSection").removeClass("active");
+    $(".man-section").click(() => {
+        if($("#womanSection").hasClass("active")){
+            $("#womanSection").removeClass("active");
+        }else if ($("#kidsSection").hasClass("active")){
+            $("#kidsSection").removeClass("active");
+        }
+
+        if($("#manSection").hasClass("active")){
+            $("#manSection").removeClass("active");
+        }else{
+            $("#manSection").addClass("active");
+        }
     });
 
-    $(".man-section").hover(() => {
-        $("#manSection").addClass("active");
+    $(".kids-section").click(() => {
+        if($("#womanSection").hasClass("active")){
+            $("#womanSection").removeClass("active");
+        }else if ($("#manSection").hasClass("active")){
+            $("#manSection").removeClass("active");
+        }
+        
+        if($("#kidsSection").hasClass("active")){
+            $("#kidsSection").removeClass("active");
+        }else{
+            $("#kidsSection").addClass("active");
+        }
     });
 
-    $(".man-section").mouseleave(() => {
-        $("#manSection").removeClass("active");
+    document.querySelector(".navbar-toggler").addEventListener("click", () => {
+        if($("#kidsSection").hasClass("active")){
+            $("#kidsSection").removeClass("active");
+        }else if ($("#manSection").hasClass("active")){
+            $("#manSection").removeClass("active");
+        } else if($("#womanSection").hasClass("active")){
+            $("#womanSection").removeClass("active");
+        }
     });
-
-    $(".kids-section").hover(() => {
-        $("#kidsSection").addClass("active");
-    });
-
-    $(".kids-section").mouseleave(() => {
-        $("#kidsSection").removeClass("active");
-    });
-
 });
+
 
 /* Navbar Script End */
 
