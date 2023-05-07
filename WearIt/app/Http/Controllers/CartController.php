e90d3d4c69623697730cd12c62b8cfe635f7848c
Matej96 <matej.skycak@gmail.com>
2023-05-07 05:11:33 +0200

    vkladanie a odoberanie z kosika bez prihlasenia

--- WearIt/app/Http/Controllers/CartController.php
+++ WearIt/app/Http/Controllers/CartController.php
@@ -10,21 +10,49 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use PhpParser\ErrorHandler\Collecting;
+use Ramsey\Collection\Collection;
 
 class CartController extends Controller
 {
     public function index(){
-        $products = DB::table('product_variations')
-            ->select('products.id', 'cart_products.id as cp_id', 'product_variation_id', 'product_name', 'size_name', 'price', 'amount', 'color_name')
-            ->join('products', 'product_variations.product_id' , '=', 'products.id')
-            ->join('cart_products', 'product_variations.id', '=', 'cart_products.product_variation_id')
-            ->join('carts', 'carts.id', '=', 'cart_products.cart_id')
-            ->join('sizes', 'sizes.id', '=', 'product_variations.size_id')
-            ->join('colors', 'colors.id','=', 'products.color_id')
-            ->where('carts.user_id', '=', auth()->user()->getAuthIdentifier())
-            ->get();
+        if (auth()->check()){
+            $products = DB::table('product_variations')
+                ->select('products.id', 'cart_products.id as cp_id', 'product_variation_id', 'product_name', 'size_name', 'price', 'amount', 'color_name')
+                ->join('products', 'product_variations.product_id' , '=', 'products.id')
+                ->join('cart_products', 'product_variations.id', '=', 'cart_products.product_variation_id')
+                ->join('carts', 'carts.id', '=', 'cart_products.cart_id')
+                ->join('sizes', 'sizes.id', '=', 'product_variations.size_id')
+                ->join('colors', 'colors.id','=', 'products.color_id')
+                ->where('carts.user_id', '=', auth()->user()->getAuthIdentifier())
+                ->get();
+
+//            dd($products);
+        } else {
+            $cart = session()->get('products', collect());
+//            session()->forget('products');
+//            session()->save();
+//            dd(session()->get('products'));
+
+            $products = collect();
+            $index = 0;
+            foreach ($cart as $product){
+                $products->push(DB::table('product_variations')
+                    ->select('products.id', DB::raw($index . ' as cp_id'), 'product_variations.id as product_variation_id', 'product_name', 'size_name', 'price', DB::raw($product['quantity'] . ' as amount'), 'color_name')
+                    ->join('products', 'product_variations.product_id' , '=', 'products.id')
+                    ->join('sizes', 'sizes.id', '=', 'product_variations.size_id')
+                    ->join('colors', 'colors.id','=', 'products.color_id')
+                    ->where('products.id', '=', $product['id'])
+                    ->where('sizes.id', '=', $product['size'])
+                    ->get());
+                $index++;
+            }
+
+            $products = $products->flatten();
+        }
 
         foreach ($products as $product) {
+//            dd($product);
             $imagePath = "images/{$product->id}-1.png";
             $publicDisk = Storage::disk('public');
 
@@ -34,8 +62,6 @@ class CartController extends Controller
                 $product->image_url = null;
             }
         }
-
-//        dd($products);
 
         $price = 0;
         foreach ($products as $product){
@@ -58,26 +84,52 @@ class CartController extends Controller
             ->pluck('quantity')->first();
 
         if ($quantity > 0 && $quantity >= $request->input('quantity')){
-            $cart_id = DB::table('carts')
-                ->select('id')
-                ->where('user_id', '=', auth()->user()->getAuthIdentifier())
-                ->value('id');
+            if (auth()->check()){
+                $cart_id = DB::table('carts')
+                    ->select('id')
+                    ->where('user_id', '=', auth()->user()->getAuthIdentifier())
+                    ->value('id');
 
-            $pv_id = DB::table('product_variations')
-                ->select('id')
-                ->where('product_id', '=', $request->input('product_id'))
-                ->where('size_id', '=', $request->input('size'))
-                ->value('id');
+                $pv_id = DB::table('product_variations')
+                    ->select('id')
+                    ->where('product_id', '=', $request->input('product_id'))
+                    ->where('size_id', '=', $request->input('size'))
+                    ->value('id');
 
-            DB::table('cart_products')->insert([
-                'cart_id' => $cart_id,
-                'product_variation_id' => $pv_id,
-                'amount' => intval($request->input('quantity'))
-            ]);
+                DB::table('cart_products')->insert([
+                    'cart_id' => $cart_id,
+                    'product_variation_id' => $pv_id,
+                    'amount' => intval($request->input('quantity'))
+                ]);
 
-            DB::table('product_variations')
-                ->where('id', '=', $pv_id)
-                ->decrement('quantity', intval($request->input('quantity')));
+                DB::table('product_variations')
+                    ->where('id', '=', $pv_id)
+                    ->decrement('quantity', intval($request->input('quantity')));
+
+            } else {
+                $products = session()->get('products', collect());
+
+                if ($products == null){
+                    $products = collect();
+                }
+
+
+                $new_product = [
+                    'id' => $request->input('product_id'),
+                    'size' => $request->input('size'),
+                    'quantity' =>$request->input('quantity')
+                ];
+
+                $products->push($new_product);
+
+                DB::table('product_variations')
+                    ->where('product_id', '=', $request->input('product_id'))
+                    ->where('size_id', '=', $request->input('size'))
+                    ->decrement('quantity', intval($request->input('quantity')));
+
+                session()->put('products', $products);
+                session()->save();
+            }
 
             return redirect()->back()->with('success', 'Produkt bol pridaný do košíka!');
         } else if($quantity == 0){
@@ -92,22 +144,41 @@ class CartController extends Controller
         $pv_id = $parts[0];
         $cp_id = $parts[1];
 
-        $amount = DB::table('cart_products')
-            ->join('carts', 'carts.id', '=', 'cart_products.cart_id')
-            ->where('carts.user_id', '=', auth()->user()->getAuthIdentifier())
-            ->where('product_variation_id', '=', $pv_id)
-            ->where('cart_products.id', '=', $cp_id)
-            ->value('amount');
-//        Log::info($amount);
+        if (auth()->check()){
+            $amount = DB::table('cart_products')
+                ->join('carts', 'carts.id', '=', 'cart_products.cart_id')
+                ->where('carts.user_id', '=', auth()->user()->getAuthIdentifier())
+                ->where('product_variation_id', '=', $pv_id)
+                ->where('cart_products.id', '=', $cp_id)
+                ->value('amount');
 
-        DB::table('product_variations')
-            ->where('id', '=', $pv_id)
-            ->increment('quantity', $amount);
-//
-        DB::table('cart_products')
-            ->where('product_variation_id', '=', $pv_id)
-            ->where('id', '=', $cp_id)
-            ->delete();
+            DB::table('product_variations')
+                ->where('id', '=', $pv_id)
+                ->increment('quantity', $amount);
+
+            DB::table('cart_products')
+                ->where('product_variation_id', '=', $pv_id)
+                ->where('id', '=', $cp_id)
+                ->delete();
+        } else {
+            $cart = session()->get('products', collect());
+
+            $pv_id = DB::table('product_variations')
+                ->where('product_id', '=', $request->input('product_id'))
+                ->where('size_id', '=', $request->input('size_id'))
+                ->value('product_variations.id');
+
+            Log:info($cart[$cp_id]);
+            $amount = $cart[$cp_id]['quantity'];
+
+            DB::table('product_variations')
+                ->where('id', '=', $pv_id)
+                ->increment('quantity', $amount);
+
+            $cart->forget($cp_id);
+            session()->put('products', $cart);
+            session()->save();
+        }
 
         return response()->json(['success' => 'Produkt bol odstránený z košíka!']);
     }
