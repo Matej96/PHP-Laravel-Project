fb4f71baa23a89d4f2feb00b2136188c2a8a5a16
Adamtomcala <xtomcala@stuba.sk>
2023-05-07 15:15:01 +0200

    Dokoncenie objednavky

--- WearIt/app/Http/Controllers/DeliveryController.php
+++ WearIt/app/Http/Controllers/DeliveryController.php
@@ -2,11 +2,103 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Country;
+use App\Models\HouseNumber;
+use App\Models\Person;
+use App\Models\Street;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DeliveryController extends Controller
 {
-    public function index(){
-        return view('delivery');
+    public function index(Request $request){
+        $validatedData = $request->input('order');
+
+
+        $products = DB::table('product_variations')
+            ->select('products.id', 'product_variation_id', 'product_name', 'size_name', 'price', 'amount', 'color_name')
+            ->join('products', 'product_variations.product_id', '=', 'products.id')
+            ->join('cart_products', 'product_variations.id', '=', 'cart_products.product_variation_id')
+            ->join('carts', 'carts.id', '=', 'cart_products.cart_id')
+            ->join('sizes', 'sizes.id', '=', 'product_variations.size_id')
+            ->join('colors', 'colors.id', '=', 'products.color_id')
+            ->where('carts.user_id', '=', auth()->user()->getAuthIdentifier())
+            ->get();
+
+        $total_price = 0;
+
+        foreach ($products as $product){
+            $total_price += $product->amount * $product->price;
+        }
+
+        $transport = DB::table('transports')
+            ->where('id', '=', $validatedData['selected_transport'])
+            ->first();
+
+        $payment = DB::table('payments')
+            ->where('id', '=', $validatedData['selected_payment'])
+            ->first();
+
+        $total_price += $transport->price;
+        $total_price += $payment->price;
+
+
+        $card_id = DB::table('carts')
+            ->select('id')
+            ->where('user_id', '=', auth()->user()->getAuthIdentifier())
+            ->value('id');
+
+        DB::table('cart_products')
+            ->where('cart_id', '=', $card_id)
+            ->delete();
+
+        $country = Country::firstOrCreate(
+            ['name' => $validatedData['country']]
+        );
+
+        $city = City::firstOrCreate(
+            ['name' => $validatedData['city']], // Podmienky na vyhľadávanie
+            ['postal_code' => $validatedData['prc']] // Hodnoty pre vytvorenie nového záznamu
+        );
+
+        $street = Street::firstOrCreate(
+            ['name' => $validatedData['street']],
+        );
+
+        $house_number = HouseNumber::firstOrCreate(
+            ['value' => $validatedData['house_number']],
+        );
+
+        $person = Person::firstOrCreate(
+            ['first_name' => $validatedData['first_name'],
+             'last_name' => $validatedData['last_name']],
+        );
+
+        $order_id = DB::table('orders')
+            ->insertGetId([
+                'user_id' => auth()->user()->getAuthIdentifier(),
+                'payment_id' => $validatedData['selected_payment'],
+                'transport_id' => $validatedData['selected_transport'],
+                'total_price' => $total_price,
+                'city_id' => $city->id,
+                'street_id' => $street->id,
+                'house_number_id' => $house_number->id,
+                'person_id' => $person->id,
+                'country_id' => $country->id,
+                'created_at' => now()
+            ]);
+
+        foreach ($products as $product){
+            DB::table('product_orders')
+                ->insert([
+                    'order_id' => $order_id,
+                    'product_variation_id' => $product->product_variation_id,
+                    'amount' => $product->amount,
+                    'price' => $product->price,
+                ]);
+        }
+
+        return redirect('/')->with('success', 'Objednávka bola vytvorená');
     }
 }
---
+++ WearIt/database/migrations/2023_05_07_123534_add_columns_to_orders.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('country_id');
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->unsignedBigInteger('city_id');
+            $table->foreign('city_id')->references('id')->on('cities');
+            $table->unsignedBigInteger('street_id');
+            $table->foreign('street_id')->references('id')->on('streets');
+            $table->unsignedBigInteger('house_number_id');
+            $table->foreign('house_number_id')->references('id')->on('house_numbers');
+            $table->unsignedBigInteger('person_id');
+            $table->foreign('person_id')->references('id')->on('people');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
